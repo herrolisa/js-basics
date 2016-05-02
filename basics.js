299a26bc99872fcb5bd58c6c86bb01f0f8610b85
Lisa <misslisazhou@gmail.com>
2016-05-01 20:53:56 -1000

    create gree function

--- basics.js
+++ basics.js
@@ -19,6 +19,9 @@ var person = {
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+ function greet(name) {
+   console.log("Hello, my name is " + name);
+ }
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
